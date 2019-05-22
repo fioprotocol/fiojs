0d6f6475eea61bd9a393ff0229472838a72d91af
james <james@librem.slim>
2019-05-22 06:42:07 -0500

    Export Ecc package.

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ import * as Numeric from './chain-numeric';
 import * as RpcInterfaces from './chain-rpc-interfaces';
 import { RpcError } from './chain-rpcerror';
 import * as Serialize from './chain-serialize';
+import * as Ecc from 'eosjs-ecc';
 
-export { Fio, Api, ApiInterfaces, Numeric, RpcInterfaces, RpcError, Serialize };
+export { Fio, Ecc, Api, ApiInterfaces, Numeric, RpcInterfaces, RpcError, Serialize };
